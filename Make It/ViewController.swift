884868223ca2951125fcf873b5b496e72d788188
Kana071727 <zushimikana0213@gmail.com>
2022-06-08 18:39:57 +0900

    Initial Commit

---
+++ Make It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Make It
+//
+//  Created by 清水　佳奈音 on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
